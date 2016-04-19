b943f8724628d44c0d236797f83d329834d53511
Omar Morales <saul090594gmail.com>
2016-04-18 23:24:51 -0500

    https

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -2,6 +2,7 @@ angular.module("MyFirstApp",[])
 
 
 .controller("FirstController", ["$scope",function(m){
+	m.image = "https://www.google.com/images/srpr/logo11w.png";
 	m.nombre = "Omar";
 	m.nuevoComentario = {};
 	m.comentarios = [
